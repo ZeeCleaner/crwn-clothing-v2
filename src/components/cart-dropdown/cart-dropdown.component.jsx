244de464a09469af47041daedd0800b0352369cd
Zeecleaner <ernsttheart@gmail.com>
2024-11-06 09:15:54 +0200

    added in addToCart feature

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,14 +1,26 @@
-import Button from '../button/button.component'
-import './cart-dropdown.styles.scss'
+import { useContext } from "react";
+
+import { CartContext } from "../../contexts/cart.context";
+
+import Button from "../button/button.component";
+
+import CartItem from "../cart-item/cart-item.component";
+
+import "./cart-dropdown.styles.scss";
 
 const CartDropdown = () => {
-    return(
-        <div className="cart-dropdown-container ">
-            <div className="cart-items"></div>
-            <Button  >Go to CheckOut</Button>
+  const { cartItems } = useContext(CartContext);
+  return (
+    <div className="cart-dropdown-container">
+      <div className="cart-items" >
+        {cartItems.map((item) => (
+            <CartItem key={item.id} cartItem={item}></CartItem>
+        ))}
+      </div>
 
-        </div>
-    )
-}
+      <Button>GO TO CHECKOUT</Button>
+    </div>
+  );
+};
 
-export default CartDropdown
+export default CartDropdown;
